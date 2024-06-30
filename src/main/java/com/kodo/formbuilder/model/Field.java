87636b78c76fe36b589399dc9696e85c7c5d25bb
Harshad Panmand <panmandharshaddev@gmail.com>
2024-06-30 16:58:37 +0530

    Breaking the infinite recursion of form and field

--- src/main/java/com/kodo/formbuilder/model/Field.java
+++ src/main/java/com/kodo/formbuilder/model/Field.java
@@ -1,5 +1,6 @@
 package com.kodo.formbuilder.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.kodo.formbuilder.model.enums.FieldType;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -7,22 +8,21 @@ import lombok.Data;
 @Data
 @Entity
 public class Field {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String label;
-
     private Boolean isRequired;
 
     @Enumerated(EnumType.STRING)
-    private FieldType type = FieldType.TEXT; // Provide a default value
+    private FieldType type = FieldType.TEXT;
 
     private String value;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "form_id")
+    @JsonBackReference // Add this to break the infinite recursion
     private Form form;
-
 }
+
--- src/main/java/com/kodo/formbuilder/model/Form.java
+++ src/main/java/com/kodo/formbuilder/model/Form.java
@@ -1,5 +1,6 @@
 package com.kodo.formbuilder.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -8,7 +9,6 @@ import java.util.List;
 @Data
 @Entity
 public class Form {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,6 +16,6 @@ public class Form {
     private String title;
 
     @OneToMany(mappedBy = "form", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference // Add this to break the infinite recursion
     private List<Field> fields;
-
 }
